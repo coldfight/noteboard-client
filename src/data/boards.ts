e7591b2d58d2c5783c9b1adc0e14a61a75345d5c
Amedeo Bonanno <abonanno@carlton.ca>
2019-07-11 07:38:04 -0400

    Add temporary data file for boards.

---
+++ src/data/boards.ts
@@ -0,0 +1,31 @@
+//  @todo: this is temporary until I build the API platform
+export default [
+  {
+    id: 1,
+    name: "Work",
+    description: "This board is for work-related stuff",
+    color: "lightblue",
+    privacy: "hidden"
+  },
+  {
+    id: 2,
+    name: "Personal",
+    description: "Personal projects and such",
+    color: "lightgreen",
+    privacy: "hidden"
+  },
+  {
+    id: 3,
+    name: "Fun",
+    description: "Some fun stuff belongs here... Yay!",
+    color: "lightyellow",
+    privacy: "hidden"
+  },
+  {
+    id: 4,
+    name: "Important",
+    description: "Important stuff goes here.",
+    color: "lightsalmon",
+    privacy: "hidden"
+  }
+];
