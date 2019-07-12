7f8417a281075b0889f0bd528862a6bb4be341a0
Amedeo Bonanno <abonanno@carlton.ca>
2019-07-12 13:29:15 -0400

    Install axios, and add .env file.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/bootstrap4-neon-glow.min.css";
 import "./assets/css/site.css";
 import Vue from "vue";
+import Axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 new Vue({
   router,
